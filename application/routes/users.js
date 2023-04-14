84bc751785b0349f112cf015cba5bfe31c7a3fc2
Unai <unai@vernelabs.cx>
2023-04-14 10:46:51 -0600

    all methods allowed

--- application/routes/users.js
+++ application/routes/users.js
@@ -9,16 +9,6 @@ let users = require('../../domain/controller/users');
 router.all('/', async (req, res) => {
     users.main(req, res)
 })
-router.post('/', async (req, res) => {
-    res.status(400).json({ message: "You need to add an id" })
-})
-//initTranscription
-router.get('/', (req, res) => {
-    users.main(req, res)
-});
-router.post('/', (req, res) => {
-    users.main(req, res)
-});
 
 router.get('/health', (req, res) => {
     const data = {
