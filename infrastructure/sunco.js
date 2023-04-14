04be0e641f121bf8438cf52c149226111b7ba95d
Unai <unai@vernelabs.cx>
2023-04-14 10:47:24 -0600

    adding env variables

--- infrastructure/sunco.js
+++ infrastructure/sunco.js
@@ -1,33 +1,32 @@
+require('dotenv').config()
 var https = require('follow-redirects').https;
 var fs = require('fs');
+const SUNCO_HOSTNAME = process.env.SUNCO_HOSTNAME
+const SUNCO_APP_ID = process.env.SUNCO_APP_ID
+const SUNCO_BASIC_TOKEN = process.env.SUNCO_BASIC_TOKEN
 
 module.exports = {
-    sendMessage(id) {
+    sendMessage(id, content) {
 
         var options = {
             'method': 'POST',
-            'hostname': 'pdi-vernelabs.zendesk.com',
-            'path': `/sc//v2/apps/62ea8e0395dd0400eff439a2/conversations/${id}/messages`,
+            'hostname': SUNCO_HOSTNAME,
+            'path': `/sc//v2/apps/${SUNCO_APP_ID}/conversations/${id}/messages`,
             'headers': {
                 'Content-Type': 'application/json',
-                'Authorization': 'Basic ',
-                'Cookie': '__cfruid=d9c7aa7f21bfdf81f05e342fd4820d42e64f4324-1681423203'
+                'Authorization': `Basic ${SUNCO_BASIC_TOKEN}`
             },
             'maxRedirects': 20
         };
-        console.log(options)
         var req = https.request(options, function (res) {
             var chunks = [];
-
             res.on("data", function (chunk) {
                 chunks.push(chunk);
             });
-
             res.on("end", function (chunk) {
                 var body = Buffer.concat(chunks);
                 console.log(body.toString());
             });
-
             res.on("error", function (error) {
                 console.error(error);
             });
@@ -37,15 +36,9 @@ module.exports = {
             "author": {
                 "type": "business"
             },
-            "content": {
-                "type": "text",
-                "text": "Hello Unai!",
-                "tags": "demo"
-            }
+            "content": content
         });
-
         req.write(postData);
-
         req.end();
     }
 }
